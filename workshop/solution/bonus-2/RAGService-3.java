ebd2c3f8ff2a1404a203067e1a98d4284fb2588b
samir <samir.kerr@gmail.com>
2025-05-28 18:00:51 +0200

    feat(): Bonus 2 with QuestionAnswerAdvisor and SafeGuardAdvisor

---
+++ workshop/solution/bonus-2/RAGService-3.java
@@ -0,0 +1,59 @@
+package fr.axa.dojo.llm.services;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+@Service
+public class RAGService {
+
+    private final RAGDataService dataService;
+    private final ChatClient chatClient;
+    private final PromptTemplate promptTemplate;
+
+    public RAGService(ChatClient.Builder builder, @Value("classpath:/prompt-system.md") Resource promptSystem, RAGDataService dataService) {
+        this.chatClient = builder
+                .defaultSystem(promptSystem)
+                .build();
+        this.dataService = dataService;
+        promptTemplate = new PromptTemplate("""
+                Answer the question based on this context:
+                {context}
+                
+                Question:
+                {question}
+                """);
+    }
+
+    public Stream<String> getResponse(final String question) {
+
+        String context = dataService.getContextForQuestion(question);
+
+        String prompt = promptTemplate.createMessage(Map.of("context", context, "question", question)).getText();
+
+        OllamaOptions options = OllamaOptions.builder()
+                .model("mistral:7b")
+                .temperature(0.1)
+                .build();
+
+        System.out.println("Preparing the answer...");
+
+        VectorStore vectorStore = dataService.getVectorStore();
+
+        return chatClient.prompt(prompt)
+                .options(options)
+                .advisors(new QuestionAnswerAdvisor(vectorStore))
+                .stream()
+                .content()
+                .toStream();
+    }
+
+}
